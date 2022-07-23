6da7842aba6a2546e1b2825043138ce5f3d67f07
hector.frasson <hector.frasson@lemondata.com.ar>
2022-07-22 23:32:39 -0300

    Modelo para controlar la aplicacion. Creamos la clase Busquedas y se bocetea el menu inicial

--- app.js
+++ app.js
@@ -1,25 +1,44 @@
-const { inquirerMenu, inquirerPausa } = require("./helpers/inquirer");
+const { inquirerMenu, inquirerPausa, leerInput } = require("./helpers/inquirer");
+const Busquedas = require("./models/busquedas");
 
 
 
 const main = async () => {
+    const busquedas = new Busquedas();
     let opcion;
 
     do {
         opcion = await inquirerMenu();
         console.log({opcion});
 
-        // switch (opcion) {
-        //     case 1:
-        //         console.log({opcion});
-        //         break;
-        //     case 2:
-        //         console.log({opcion});
-        //         break;
-        //     case 0:
-        //         console.log({opcion});
-        //         break;
-        // }
+        switch (opcion) {
+            case 1:
+                //Mostrar mensaje para que el usuario escriba
+                const lugar = await leerInput('Ciudad: ');
+                console.log(lugar);
+                
+                //Buscar los lugares
+
+                //Seleccionar el lugar
+
+                //Datos del clima de la ciudad
+
+                //Mostrar resultados
+                console.log('\nInformacion del lugar\n'.green);
+                console.log('Ciudad: ')
+                console.log('Latitud: ')
+                console.log('Longitud: ')
+                console.log('Temperatura: ')
+                console.log('Temp. Minima: ')
+                console.log('Temp. Maxima: ')
+                break;
+            case 2:
+                console.log({opcion});
+                break;
+            case 0:
+                console.log({opcion});
+                break;
+        }
 
         console.log('\n');
         await inquirerPausa();
---
+++ models/busquedas.js
@@ -0,0 +1,21 @@
+
+
+
+class Busquedas {
+    
+    historial = ['Madrid', 'Tegucigalpa', 'San José'];
+
+
+    constructor(){
+        //TODO: Leer db si existe
+    }
+
+    async ciudad(lugar = ''){
+        //peticion http
+        console.log(lugar);
+
+        return []; //retornar los lugares que coincidan con el lugar ingresado por el usuario.
+    }
+}
+
+module.exports = Busquedas;
